d57c51829eeec31bb8894f4d18f5d3bedbf6f341
Administrator <nguyenhuynhcongly17092000@gmail.com>
2021-09-15 20:11:35 +0700

    Initial commit

---
+++ src/main/java/com/iuh/repository/EmployeeRepo.java
@@ -0,0 +1,9 @@
+package com.iuh.repository;
+
+import com.iuh.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+}
---
+++ src/main/java/com/iuh/service/ServiceInterface.java
@@ -0,0 +1,11 @@
+package com.iuh.service;
+
+import com.se.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ServiceInterface {
+    public List<Employee> findAll();
+}
